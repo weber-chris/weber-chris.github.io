11af1ecb2c62949db7592c0fce8f46a454d541e5
Christoph Weber <christoph.t.weber@gmail.com>
2019-11-08 00:31:07 +0100

    first layout version with placeholders for all content

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ function clickcountry(myid) {
     return false;
 }
 
+function set_star_rating(star_rating) {
+    rating = document.getElementById("rating_div");
+    let stars = rating.getElementsByTagName("span")
+    for (i = 0; i < star_rating; i++) {
+        stars[i].classList.add("fa-star")
+    }
+    for (i = star_rating; i < 5; i++) {
+        stars[i].classList.add("fa-star-o")
+    }
+}
+
+function extract_from_goodreads(goodreads_url){
+    // TODO
+    // goodreads API https://www.goodreads.com/api
+// fetch(goodreads_url);
+// (async () => {
+//     const response = await fetch(goodreads_url);
+//     const text = await response.text();
+//     console.log(text.match(/(?<=\<h1>).*(?=\<\/h1>)/));
+//   })()
+}
+
 function on_country_click(e, divid, country_id) {
     let left = e.clientX + "px";
     let top = e.clientY + "px";
@@ -24,7 +46,8 @@ function on_country_click(e, divid, country_id) {
 
     // $country_details.find('#country_name').html(country_name_text);
     // book_metadata = country_details.getElementById("book_metadata");
-
+    let goodreads_url = "https://www.goodreads.com/book/show/3613997-the-associate";
+    extract_from_goodreads(goodreads_url);
     // Metadata
     country_name = document.getElementById("country_name");
     country_name.textContent = country_name_text;
@@ -33,14 +56,12 @@ function on_country_click(e, divid, country_id) {
     book_author = document.getElementById("book_author");
     book_author.textContent = "John Grisham";
 
-    // Review
-    goodreads_link = document.getElementById("goodreads_link");
-    goodreads_link.textContent = "Link";
-    reading_status = document.getElementById("reading_status");
-    reading_status.textContent = "Read";
-    // scrape from goodreads
-    rating = document.getElementById("rating");
-    rating.textContent = "4/5";
+    // Review, Scrape from goodreads
+    avg_rating = document.getElementById("avg_rating");
+    avg_rating.textContent = "(⌀3.8)";
+    // reading_status = document.getElementById("reading_status");
+    // reading_status.textContent = "Read";
+    set_star_rating(2);
 
     //Summary
     book_summary_text = document.getElementById("book_summary_text");
